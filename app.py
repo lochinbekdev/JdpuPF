33e9839c9c02f728d37f717b31c40078fa71974c
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-05-07 11:15:33 +0500

    make request and response use from webob

---
+++ app.py
@@ -0,0 +1,16 @@
+from webob import Request, Response
+
+class PPFApp:
+
+    def __call__(self, environ, start_response):
+        request=Request(environ)
+        response=self.handle_request(request)
+        return response(environ,start_response)
+    
+
+    def handle_request(self,request):
+        user_agent=request.environ.get("HTTP_USER_AGENT","User Agent Not Found")
+        response=Response()
+        response.text=f"Hello my friend with user agent {user_agent}"
+
+        return response
