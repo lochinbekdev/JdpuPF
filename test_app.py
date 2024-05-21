192b25385ae357134dfa4c3dc1291750c9c6976a
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2024-05-21 10:43:31 +0500

    unit testing jdpuPy frame work

--- test_app.py
+++ test_app.py
@@ -48,3 +48,24 @@ def test_class_based_get(app,test_client):
             resp.text = "Books page"
             
     assert test_client.get("http://testserver/books").text == "Books page"
+    
+def test_class_based_post(app,test_client):
+    @app.route("/books")
+    class Books:
+        def post(self,req,resp):
+            resp.text = "Endpoint to create a book"
+            
+    assert test_client.post("http://testserver/books").text == "Endpoint to create a book"
+    
+    
+def test_class_based_method_not_allowed(app,test_client):
+    @app.route("/books")
+    class Books:
+        def post(self,req,resp):
+            resp.text = "Endpoint to create a book"
+            
+    response = test_client.get("http://testserver/books")
+    
+    assert response.text == "Method Not Allowed"
+    assert response.status_code == 405
+    
